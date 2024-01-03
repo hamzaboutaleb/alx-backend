ea279d8392ed77461df5ffa1198b1901af9f07d7
hamzaboutaleb <hamza-boutaleb@live.fr>
2024-01-04 00:24:46 +0100

    task 1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python3
-"""LIFO Caching"""
+"""FIFO caching"""
 
 from base_caching import BaseCaching
 
 
-class LIFOCache(BaseCaching):
-    """caching system"""
+class FIFOCache(BaseCaching):
+    """Caching system"""
 
     def put(self, key, item):
-        """Add new item"""
+        """Add a new item"""
         if key is not None and item is not None:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                discarded_key = list(self.cache_data.keys())[-1]
+                discarded_key = next(iter(self.cache_data))
                 del self.cache_data[discarded_key]
                 print(f"DISCARD: {discarded_key}")
-
             self.cache_data.update({key: item})
 
     def get(self, key):
-        """Get item by key"""
+        """get item by key"""
         return self.cache_data.get(key)
