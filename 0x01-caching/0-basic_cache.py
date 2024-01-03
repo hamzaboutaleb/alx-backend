b2771cad6f50ced7126305ee4f04f829b0a9922b
hamzaboutaleb <hamza-boutaleb@live.fr>
2024-01-04 00:09:22 +0100

    task 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Basic Cache"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Cache"""
+
+    def put(self, key, item):
+        """add new item to cache"""
+
+        if key is not None and item is not None:
+            self.cache_data.update({key: item})
+
+    def get(self, item):
+        """get cached item"""
+        return self.cache_data.get(item)
